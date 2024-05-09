ca5e70d80d82789bcf9bbe73d14c37a6f669762e
Thiago Felipe <thiagof.g3@gmail.com>
2024-05-09 18:11:05 -0300

    test: validando se o ID do video não for um UUID

--- encoder/domain/video_test.go
+++ encoder/domain/video_test.go
@@ -3,12 +3,26 @@ package domain_test
 import (
 	"encoder/domain"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestValidateIfVideoIsEmpty(t *testing.T) {
 	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIdIsnotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "any_ID"
+	video.ResourceID = "any_resourceID"
+	video.FilePath = "any_path"
+	video.CreatedAt = time.Now()
+
 	err := video.Validate()
 
 	require.Error(t, err)
