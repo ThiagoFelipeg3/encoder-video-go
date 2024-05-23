1ffb9bc8863ecb2def1288997e0da3334f6cbd0d
Thiago Felipe <thiagof.g3@gmail.com>
2024-05-23 13:01:18 -0300

    test: validando a atualizacao de um job no banco

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -34,3 +34,29 @@ func TestJobRepositoryDbInsert(t *testing.T) {
 	require.Equal(t, jobFind.ID, job.ID)
 	require.Equal(t, jobFind.VideoID, video.ID)
 }
+
+func TestJobRepositoryDbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+	require.Nil(t, err)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	job.Status = "Complete"
+	repoJob.Update(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.Status, job.Status)
+}
