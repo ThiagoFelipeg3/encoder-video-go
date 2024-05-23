7be88e4ba285ef5d735438a1452e6fa142a9dbf6
Thiago Felipe <thiagof.g3@gmail.com>
2024-05-23 12:58:41 -0300

    test: testando o metodo Insert do JobRepository

---
+++ application/repositories/job_repository_test.go
@@ -0,0 +1,36 @@
+package repositories_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/domain"
+	"encoder/framework/database"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestJobRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("any_putput_path", "Peding", video)
+	require.Nil(t, err)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	_, errJob := repoJob.Insert(job)
+	require.Nil(t, errJob)
+
+	jobFind, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, jobFind.ID)
+	require.Nil(t, err)
+	require.Equal(t, jobFind.ID, job.ID)
+	require.Equal(t, jobFind.VideoID, video.ID)
+}
